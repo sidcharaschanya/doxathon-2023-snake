11c34959a2ee45cbb6d46b492e81ddee6c6fdba7
Sid Charaschanya <43777196+sidcharaschanya@users.noreply.github.com>
2023-03-12 13:47:55 +0000

    third commit

--- submission/agent.py
+++ submission/agent.py
@@ -1,15 +1,15 @@
-import math
 from typing import List, Optional
 
 from snake import Action, BaseAgent, CellState, main, BOARD_SIZE
 
 from collections import deque
 
-
 #################################################################
 #   Modify the Agent class below to implement your own agent.   #
 #   You may define additional methods as you see fit.           #
 #################################################################
+
+Position = tuple[int, int]
 
 
 class Agent(BaseAgent):
@@ -17,14 +17,26 @@ class Agent(BaseAgent):
 
     def __init__(self):
         super().__init__()
+        self.dist_to_source: Optional[List[List[int]]] = None
+        self.edge_to: Optional[List[List[Position]]] = None
+        self.reset()
+
+    def reset(self):
         self.dist_to_source = [[-1 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-        self.edge_to = [[-1 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        self.edge_to = [[(-1, -1) for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
     @staticmethod
-    def __norm(pos: tuple[int, int]):
-        return math.sqrt(pos[0] ** 2 + pos[1] ** 2)
+    def __pos(board: List[List[CellState]], cell_state: CellState):
+        for i, row in enumerate(board):
+            for j, state in enumerate(row):
+                if state == cell_state:
+                    return i, j
 
-    def bfs(self, board: List[List[CellState]], source: tuple[int, int]):
+    @staticmethod
+    def __diff(a: Position, b: Position):
+        return (a[0] - b[0]) % BOARD_SIZE, (a[1] - b[1]) % BOARD_SIZE
+
+    def bfs(self, board: List[List[CellState]], source: Position):
         queue = deque()
         queue.append(source)
 
@@ -32,10 +44,8 @@ class Agent(BaseAgent):
             vertex = queue.popleft()
 
             for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                row, col = vertex[0] + dr, vertex[1] + dc
-
-                if row < 0 or col < 0 or row > BOARD_SIZE - 1 or col > BOARD_SIZE - 1:
-                    continue
+                row = (vertex[0] + dr) % BOARD_SIZE
+                col = (vertex[1] + dc) % BOARD_SIZE
 
                 if board[row][col] == CellState.TAIL:
                     continue
@@ -45,15 +55,29 @@ class Agent(BaseAgent):
                     self.dist_to_source[row][col] = self.dist_to_source[vertex[0]][vertex[1]] + 1
                     self.edge_to[row][col] = vertex
 
-    def shortest_path_to(self, vertex, source) -> Optional[tuple[int, int]]:
-        if not self.dist_to_source[vertex[0]][vertex[1]] != -1:
+    def shortest_path_to(self, vertex: Position, source) -> Optional[Position]:
+        if self.dist_to_source[vertex[0]][vertex[1]] == -1:
             return None
+
         path = []
         curr = vertex
+
         while curr != source:
             path.append(curr)
             curr = self.edge_to[curr[0]][curr[1]]
+
         return path[-1]
+
+    @staticmethod
+    def act(step):
+        if step[0] == 1:
+            return Action.DOWN
+        elif step[0] == -1:
+            return Action.UP
+        elif step[1] == 1:
+            return Action.RIGHT
+        else:
+            return Action.LEFT
 
     async def make_move(self, board: List[List[CellState]]) -> Action:
         """Makes a move.
@@ -62,33 +86,16 @@ class Agent(BaseAgent):
         Returns:
             Action: The direction to move in.
         """
-        head, fruit = None, None
-
-        for i, row in enumerate(board):
-            for j, cell_state in enumerate(row):
-                if cell_state == CellState.HEAD:
-                    head = (i, j)
-
-                if cell_state == CellState.FRUIT:
-                    fruit = (i, j)
+        head = Agent.__pos(board, CellState.HEAD)
+        fruit = Agent.__pos(board, CellState.FRUIT)
 
         self.bfs(board, head)
         cell = self.shortest_path_to(fruit, head)
 
-        move = (cell[0] - head[0], cell[1] - head[1])
+        step = Agent.__diff(cell, head)
+        self.reset()
 
-        if Agent.__norm((fruit[0] - head[0], fruit[1] - head[1])) == 1:
-            self.dist_to_source = [[-1 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-            self.edge_to = [[-1 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-
-        if move[0] == 1:
-            return Action.DOWN
-        elif move[0] == -1:
-            return Action.UP
-        elif move[1] == 1:
-            return Action.RIGHT
-        else:
-            return Action.LEFT
+        return self.act(step)
 
 
 if __name__ == "__main__":
